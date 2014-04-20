dca80a6e3949fedc883d852ff18f5057994478d7
aerobless <theo.winter@gmail.com>
2014-04-20 17:00:24 +0200

    cleaned up code, removed .useQuaternion because it's default anyway

--- js/PointerLockControlsFPS.js
+++ js/PointerLockControlsFPS.js
@@ -61,46 +61,6 @@
         pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, pitchObject.rotation.x ) );
     };
 
-/*
-THREE.PointerLockControls = function ( camera ) {
-
-	var scope = this;
-
-	camera.rotation.set( 0, 0, 0 );
-
-	var pitchObject = new THREE.Object3D();
-	pitchObject.add( camera );
-
-	var yawObject = new THREE.Object3D();
-	yawObject.position.y = 10;
-	yawObject.add( pitchObject );
-
-	var moveForward = false;
-	var moveBackward = false;
-	var moveLeft = false;
-	var moveRight = false;
-
-	var isOnObject = false;
-	var canJump = false;
-
-	var velocity = new THREE.Vector3();
-
-	var PI_2 = Math.PI / 2;
-
-	var onMouseMove = function ( event ) {
-
-		if ( scope.enabled === false ) return;
-
-		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-
-		yawObject.rotation.y -= movementX * 0.002;
-		pitchObject.rotation.x -= movementY * 0.002;
-
-		pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, pitchObject.rotation.x ) );
-
-	};
-*/
 	var onKeyDown = function ( event ) {
 
 		switch ( event.keyCode ) {
@@ -176,12 +136,7 @@ THREE.PointerLockControls = function ( camera ) {
 	this.getObject = function () {
 		return yawObject;
 	};
-/*
-	this.isOnObject = function ( boolean ) {
-		isOnObject = boolean;
-		canJump = boolean;
-	};
-*/
+
 	this.getDirection = function() {
 		// assumes the camera itself is not rotated
 		var direction = new THREE.Vector3( 0, 0, -1 );
@@ -237,43 +192,4 @@ THREE.PointerLockControls = function ( camera ) {
 
         cannonBody.position.copy(yawObject.position);
     };
-
-/*	this.update = function ( delta ) {
-
-		if ( scope.enabled === false ) return;
-
-		delta *= 0.1;
-
-		velocity.x += ( - velocity.x ) * 0.08 * delta;
-		velocity.z += ( - velocity.z ) * 0.08 * delta;
-
-		velocity.y -= 0.25 * delta;
-
-		if ( moveForward ) velocity.z -= 0.12 * delta;
-		if ( moveBackward ) velocity.z += 0.12 * delta;
-
-		if ( moveLeft ) velocity.x -= 0.12 * delta;
-		if ( moveRight ) velocity.x += 0.12 * delta;
-
-		if ( isOnObject === true ) {
-
-			velocity.y = Math.max( 0, velocity.y );
-
-		}
-
-		yawObject.translateX( velocity.x );
-		yawObject.translateY( velocity.y );
-		yawObject.translateZ( velocity.z );
-
-		if ( yawObject.position.y < 10 ) {
-
-			velocity.y = 0;
-			yawObject.position.y = 10;
-
-			canJump = true;
-
-		}
-
-	};*/
-
 };
