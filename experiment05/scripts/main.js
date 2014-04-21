ee03ef8a64bfb72cb2cc9d751cacb952cdc02590
aerobless <theo.winter@gmail.com>
2014-04-21 11:05:21 +0200

    working on experiment 5, better project structure with require.js and a module hierarchy

---
+++ experiment05/scripts/main.js
@@ -0,0 +1,17 @@
+prompt("hello world");
+console.log("ddd");
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'scripts/lib',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        app: '../app',
+        helpers: '../helpers'
+    }
+});
+
+prompt("hello world");
